696fe48ddb36e2ceb0c5ef1444e7ae4158670035
Bruno Russo <btuno.russo@adventhealth.com>
2022-01-20 15:31:31 -0300

    added auth

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "amazonamplifycd11b0d4": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { withAuthenticator, Authenticator } from "@aws-amplify/ui-react";
+
 import logo from "./logo.svg";
 import "./App.css";
 
@@ -8,8 +10,16 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>What's up guys?</p>
       </header>
+      <Authenticator>
+        {({ signOut, user }) => (
+          <main>
+            <h1>Hello {user.username}</h1>
+            <button onClick={signOut}>Sign out</button>
+          </main>
+        )}
+      </Authenticator>
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
